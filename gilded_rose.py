68ea8ea32d520ccc66cd5e532033beac8e2b41e7
GuanHong Jiang <guanhongjiang@login.khoury.northeastern.edu>
2024-02-24 14:37:27 -0800

    final update

--- gilded_rose.py
+++ gilded_rose.py
@@ -20,30 +20,40 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            # Check if the item is "Conjured"
+            if item.name.startswith("Conjured"):
+                degrade_rate = 2
+            else:
+                degrade_rate = 1
+
+            # Handle quality degradation for normal and special items
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality -= degrade_rate
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality += 1
                         if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality += 1
+
+            # Update sell_in value
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
+
+            # Handle quality change after the sell-by date has passed
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality -= degrade_rate
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
+
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,47 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+
+    def test_conjured_item_quality_degrades_twice_as_fast(self):
+        conjured_item_name = "Conjured Mana Cake"
+        items = [Item(conjured_item_name, 3, 6)]  # Starting with 3 days to sell and quality 6
+        gr = GildedRose(items)
+
+        gr.update_quality()  # After 1 day
+
+        # Quality should degrade by 2, SellIn by 1
+        self.assertEqual(items[0].quality, 4)
+        self.assertEqual(items[0].sell_in, 2)
+
+        gr.update_quality()  # After 2 days
+
+        # Quality should degrade by another 2, SellIn by another 1
+        self.assertEqual(items[0].quality, 2)
+        self.assertEqual(items[0].sell_in, 1)
+
+    def test_conjured_item_quality_degrades_four_times_as_fast_after_sell_by_date(self):
+        conjured_item_name = "Conjured Mana Cake"
+        items = [Item(conjured_item_name, 0, 6)]  # Sell by date has passed, quality 6
+        gr = GildedRose(items)
+
+        gr.update_quality()  # After sell by date
+
+        # Quality should degrade by 4 as sell by date has passed
+        self.assertEqual(items[0].quality, 2)
+
+    def test_vest_item_should_decrease_after_one_day(self):
+        vest = "+5 Dexterity Vest"
+        items = [Item(vest, 1, 2), Item(vest, 9, 19), Item(vest, 4, 6)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        expected_items = [Item(vest, 0, 1), Item(vest, 8, 18), Item(vest, 3, 5)]
+        for i, item in enumerate(gr.items):
+            with self.subTest(i=i):
+                self.assertEqual(item.name, expected_items[i].name)
+                self.assertEqual(item.sell_in, expected_items[i].sell_in)
+                self.assertEqual(item.quality, expected_items[i].quality)
 
 
 if __name__ == '__main__':
